664f5e267633bdc40cd6a71d6efe324fa48b2593
Sergio Garcia Prada <sergiogarcia@ou.edu>
2020-10-30 10:13:19 -0500

    myNRML function added

---
+++ R/myNRML.R
@@ -0,0 +1,39 @@
+#' myNRML (My newton raphson maximum likelihood)
+#'
+#' @param x0 start value to start finding the inflexion point of the function
+#' @param delta step ofthe estimation (the smaller this is, the accurate will be the answer)
+#' @param llik  log function of the data of interest
+#' @param xrange range of the log lik function, the interval where the maximum should be found
+#' @param parameter name of the parameter of interest
+#'
+#' @return returns a graph of the loglik function and a graph of the netwon raphson algorithm of the derivative. Also returns a table with the steps of the approximation until finding the maximum
+#' @export
+#'
+#' @examples
+myNRML=function(x0,delta=0.001,llik,xrange,parameter="param"){
+  f=function(x) (llik(x+delta)-llik(x))/delta
+  fdash=function(x) (f(x+delta)-f(x))/delta
+  d=1000
+  i=0
+  x=c()
+  y=c()
+  x[1]=x0
+  y[1]=f(x[1])
+  while(d > delta & i<100){
+    i=i+1
+    x[i+1]=x[i]-f(x[i])/fdash(x[i])
+    y[i+1]=f(x[i+1])
+    d=abs(y[i+1])
+  }
+  layout(matrix(1:2,nr=1,nc=2,byrow=TRUE),width=c(1,2))
+  curve(llik(x), xlim=xrange,xlab=parameter,ylab="log Lik",main="Log Lik")
+  curve(f(x),xlim=xrange,xaxt="n", xlab=parameter,ylab="derivative",main=  "Newton-Raphson Algorithm \n on the derivative")
+  points(x,y,col="Red",pch=19,cex=1.5)
+  axis(1,x,round(x,2),las=2)
+  abline(h=0,col="Red")
+
+  segments(x[1:(i-1)],y[1:(i-1)],x[2:i],rep(0,i-1),col="Blue",lwd=2)
+  segments(x[2:i],rep(0,i-1),x[2:i],y[2:i],lwd=0.5,col="Green")
+
+  list(x=x,y=y)
+}
